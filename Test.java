da57f65ccfa81f1ee9b351cc66ac518b88a55bad
nk10008 <nkgupta3541@gmail.com>
2022-03-30 12:02:20 +0530

    Memory Test Example

---
+++ Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+
+	public static void main(String args[]) {
+		Test Test = new Test();
+		Test.start();
+	}
+	
+	public void start() {
+		String last = "Z";
+		Container container = new Container();
+		container.setInitial("C");
+		another(container,last);
+		System.out.print(container.getInitial());
+	}
+	
+	public void another(Container initialHolder, String newInitial) {
+		newInitial.toLowerCase();
+		initialHolder.setInitial("B");
+		Container initial2 = new Container();
+		initialHolder=initial2;
+		System.out.print(initialHolder.getInitial());
+		System.out.print(newInitial);
+	}
+}
